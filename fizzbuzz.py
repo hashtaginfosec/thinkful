1411ce71a08fe998aaa4aa13ae64ae78942e7555
qasimchadhar <qasimchadhar@users.noreply.github.com>
2014-07-01 04:09:13 -0500

    Fizzbuzz version 2
    Allows users to pass upper limit through sys argv input or standard input

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,15 @@
-num = 100
+#fizbuzz v2
+
+import sys
+num = 0
+
+if len(sys.argv) <= 1:
+    num = int(input("Please type upper limit >>> "))
+
+else:
+    for arg in sys.argv[1:]:
+        num = int(arg)
+
 print("Fizz buzz counting up to " + str(num))
 for i in range(1, num+1):
 
@@ -10,3 +21,4 @@ for i in range(1, num+1):
         print("fizz buzz")
     if (i % 3 != 0) and (i % 5 != 0):
         print(i)
+
