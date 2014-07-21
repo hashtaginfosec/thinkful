14404af2a2590e9a6519c506f6085c6b10d1bd63
qasimchadhar <qasimchadhar@users.noreply.github.com>
2014-07-21 06:13:05 -0500

    Update bicycle.py

--- bicycle.py
+++ bicycle.py
@@ -26,35 +26,48 @@ class Manufacturers(object):
         self.percent = percent
 
 
-#This class will construct a bike
 class Bicycles(object):
     def __init__(self, name, manufacturer, frame, wheel):
 
-        self.weight = Frames.weight + Wheels.weight * 2
-        self.cost = self.frame.cost + self.wheel.cost * 2
         self.name = name
         self.wheel = wheel
         self.frame = frame
+        self.weight = self.frame.weight + self.wheel.weight * 2
+        self.cost = self.frame.cost + self.wheel.cost * 2
         self.manufacturer = manufacturer
+
+    #add manufacturer's percentage to wholesale cost
+    # def wholesale_cost(self):
+    #     return self.cost + (self.cost // 100) * self.manufacturer.percent
+    #
+    # def retail_cost(self, shop):
+    #
+    #      return self.cost + ((self.cost // 100) * self.manufacturer.percent) + ((self.cost // 100) * martys.margin)
 
 
 class Shops(object):
     def __init__(self, name, margin):
         self.inventory = []
-        self.margin = margin
+        self.margin = 20
         self.name = name
-        self.profit = 0  #we start with no profit
+        self.profit = 0  # we start with no profit
 
     #Now we create bikes for inventory that will be set by bicycleScript.py
     def add_inventory(self, bicycle):
-
         self.inventory.append(bicycle)
 
 
-
 class Customers(object):
-    def __init__(self, name, fund, bikes):
+    def __init__(self, name, fund):
         self.name = name
         self.fund = fund
         self.bikes = []
+
+    def buy_bike(self, bicycle):
+        self.bikes.append(bicycle)
+        print("add to customer inventory: " + bicycle)
+
+
+
+
 
